a5db1f81bc4ec40713c4d2106853f0eab31a3c50
crazybahna <40315064+crazybahna@users.noreply.github.com>
2018-06-17 18:51:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 
 const fs = require("fs"); 
-
+bot.login(process.env.BOT_TOKEN);
 
 bot.on('message', function (message){
 	if(message.content ==='!ibahna'){
 		message.reply('Ouiiiiiiii ?');
 	} 
-bot.login(process.env.BOT_TOKEN)
  
 });
