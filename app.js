d54dff65805e84f0f18822efdf55359e2e86bfcd
crazybahna <40315064+crazybahna@users.noreply.github.com>
2018-06-17 13:02:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const fs = require("fs");
 
 bot.on('message', function (message){
 	if(message.content ==='!ibahna'){
-		message.reply('Ouiiiiiiii ?');
+		message.reply('Ouiiiiiiii ?')
 	} 
-bot.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN)
  
-});
+})
