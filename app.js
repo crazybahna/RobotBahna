5a68c7284dde544f72714536755f3f7603fc135c
crazybahna <40315064+crazybahna@users.noreply.github.com>
2018-06-17 18:55:35 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ bot.login(process.env.BOT_TOKEN);
 
 bot.on('message', function (message){
 	if(message.content ==='!ibahna'){
-		message.reply('Ouiiiiiiii ?');
+		message.reply('Exec RoboBahna');
 	} 
  
 });
